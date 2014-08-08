9ae7686275ce5360b5b73a6e1606404ed378c551
barooahn <barooahn@gmail.com>
2014-08-08 18:32:50 +0800

    new email

--- app/views/emails/auth/welcome.blade.php
+++ app/views/emails/auth/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en-US">
     <body>
-        Hi there, {{ $user->name }}. Thanks again for signing up for ELTDP! Please click <a href="http://http://funny-florentina-02.gopagoda.com/confirm?email={{$user->email}}&key={{$key}}"> here </a>' to validate your account.
+        Hi there, {{ $user->name }}. Thanks again for signing up for ELTDP! Please click <a href="http://abashed-albina.gopagoda.com/confirm?email={{$user->email}}&key={{$key}}"> here </a>' to validate your account.
  
         Thanks,
         Management
