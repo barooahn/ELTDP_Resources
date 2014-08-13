52e35aff3ea3318ca517e8e9e09b96065d208db8
barooahn <barooahn@gmail.com>
2014-08-13 18:21:38 +0800

    delted libre office and resources controller

--- public/test.php
+++ public/test.php
@@ -1,14 +1,1 @@
-<?php 
-//first copy the file to the magic place where we can convert it to a pdf on the fly
-copy('test.docx', "LibreOffice2/cde-package/cde-root/home/robert/Desktop/".'test.docx');
-//change to that directory
-chdir('/');
-//the magic command that does the conversion
-$myCommand = "./libreoffice.cde --headless -convert-to pdf Desktop/".'test.docx'." -outdir Desktop/";
-exec ($myCommand);
-//copy the file back
-
-//changing the header to the location of the file makes it work well on androids
-header( 'Location: '.str_replace(".doc", ".pdf", 'test.docx') );
-?>
-?>
+test.php
