a99ebf8cb422297414115955e741f59e39dd5106
barooahn <barooahn@gmail.com>
2014-08-13 16:58:30 +0800

    removed unlink word

--- app/models/Resource.php
+++ app/models/Resource.php
@@ -73,8 +73,7 @@ class Resource extends \Eloquent {
                     chdir('LibreOffice2/cde-package/cde-root/home/robert/');
                     exec("./libreoffice.cde --headless -convert-to pdf $filename.$extension");
                     exec("convert -density 200 $filename.pdf[0] ../../eltdpPictures/$filename.jpg");
-                    unlink($filename .'.'.$extension);
-                    unlink($filename .'.pdf');
+
                     $pathToPicture = 'eltdpPictures/'. $filename .'.jpg';
                     break;
                 }
