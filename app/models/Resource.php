528b50bb1c8e3a3f2e8a5814f5edc2e0cc2aed4c
barooahn <barooahn@gmail.com>
2014-08-18 21:10:54 +0800

    Test word again

--- app/models/Resource.php
+++ app/models/Resource.php
@@ -85,7 +85,7 @@ class Resource extends \Eloquent {
 
                 if(copy($file, $newfile)) {
                     chdir('LibreOffice2/cde-package/cde-root/home/robert/');
-                    exec("libreoffice.cde --headless -convert-to pdf $filename.$extension");
+                    exec("./libreoffice.cde --headless -convert-to pdf ".$filename.$extension);
                     exec("convert -density 200 $filename.pdf[0] ../../eltdpPictures/$filename.jpg");
                     if (file_exists ( 'eltdpPictures/'. $filename .'.jpg'))
                     { 
